8af160ae341311aa06597e3dee6ea30ea50d9517
Afrasiyab Nangyal Kakakhel <afrasiyabkk@gmail.com>
2025-07-17 14:38:08 +0500

    in progerss

--- licensingapp/models.py
+++ licensingapp/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from project.commons.common_constants import DurationType
 
 class LicenseType(models.Model):
     duration = models.IntegerField()
+    duration_type = models.CharField(
+        max_length=10,
+        choices=[(tag.value, tag.name) for tag in DurationType],
+        default=DurationType.MONTHS.value
+    )
     price_per_user = models.DecimalField(max_digits=10, decimal_places=2)
     name = models.CharField(max_length=255)
 
---
+++ project/commons/common_constants.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class DurationType(Enum):
+    DAYS = 'days'
+    MONTHS = 'months'
+    YEARS = 'years'
